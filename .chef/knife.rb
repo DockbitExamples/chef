ac7e75030760bf15b682e8608945462b071e0f13
Tair Assimov <tair@dockbit.com>
2015-12-09 10:27:43 +0100

    Point client_key to client.pem in home directory

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,6 +5,7 @@ log_level                :info
 log_location             STDOUT
 node_name                "dockbit"
 validation_client_name   "dockbit-validator"
+client_key               "#{ENV['HOME']}/.chef/client.pem"
 validation_key           "#{current_dir}/dockbit-validator.pem"
 chef_server_url          "https://api.chef.io/organizations/dockbit"
 cookbook_path            ["#{current_dir}/../cookbooks"]
